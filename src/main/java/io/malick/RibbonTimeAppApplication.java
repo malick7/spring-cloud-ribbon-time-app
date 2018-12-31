5bdf561f576e7e9dd9eacf4be755e8e1f500ce9c
saleem.pm <saleem.pm@tavant.com>
2018-12-31 20:30:01 +0530

    CSLB without Discovery service - RibbonClient + LoadBalanced annotation

--- src/main/java/io/malick/RibbonTimeAppApplication.java
+++ src/main/java/io/malick/RibbonTimeAppApplication.java
@@ -6,13 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@EnableDiscoveryClient
+@RibbonClient(name = "time-service")
 @SpringBootApplication
 public class RibbonTimeAppApplication {
 	
